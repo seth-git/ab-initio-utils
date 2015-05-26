2df9e6b6421cf27e457f610d1da46ba5cc164242
seth-git <sethcall2@gmail.com>
2015-05-25 23:47:30 -0600

    Updated the documentation.

--- js/controller.js
+++ js/controller.js
@@ -1,36 +1,45 @@
 (function(appName) {
-	"use strict";
+  "use strict";
   angular
     .module(appName, [])
     .controller('Controller', Controller);
 
-	var pages = [
-		{
-			key: 'intro',
-			title: 'Introduction'
-		},
-		{
-			key: 'install',
-			title: 'Installation Instructions'
-		}
-	];
+  var pages = [
+    {
+      key: 'intro',
+      title: 'Introduction'
+    },
+    {
+      key: 'input',
+      title: 'Input Files'
+    },
+    {
+      key: 'install',
+      title: 'Installation Instructions'
+    }
+  ];
 
-  function Controller() {
+  Controller.$inject = ['$window','$timeout'];
+  function Controller($window, $timeout) {
     var vm = this;
 
-		vm.toggleShow = toggleShow;
+    vm.toggleShow = toggleShow;
 
-		vm.pages = pages;
-		toggleShow(vm.pages[0]);
+    vm.pages = pages;
+    toggleShow(vm.pages[0]);
 
-		function toggleShow(page) {
-			page.show = !page.show;
-			if (page.show)
-				vm.pages.forEach(function (pg) {
-					if (pg !== page)
-						pg.show = false;
-				});
-		}
+    function toggleShow(page) {
+      page.show = !page.show;
+      if (page.show) {
+        vm.pages.forEach(function (pg) {
+          if (pg !== page)
+            pg.show = false;
+        });
+        $timeout(function() {
+          $window.location.href = '#' + page.key;
+        });
+      }
+    }
   }
 }('ab-initio-utils'));
 
